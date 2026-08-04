61206c8e5b070a4871d3723f9212bca94b384702
Arnis Vuskans <ampatspell@gmail.com>
2026-08-04 23:09:20 +0300

    initial import

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div class="page">Index</div>
+
+<style lang="scss">
+	.page {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 10px;
+	}
+</style>
